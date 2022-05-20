0b8c2dc2fbdcd3e32f8ab24f94e10dd1afdbf444
TheLeleo <99210769+TheLeleo@users.noreply.github.com>
2022-05-20 09:14:53 -0300

    Add files via upload

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -1,8 +1,24 @@
+// Adicionar novos itens na aba de projetos
+const projects = document.querySelector(".projects_section");
+
 let darkToggle = document.querySelector(".dark");
 let darkMode = document.querySelector(".dark_change");
 var change = 0;
 
+
+load_functions();
+
+
+
+function load_functions(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+    });
+}
+
 darkToggle.onclick = function() {
+   
+
     if (change == 0){
         darkMode.classList.add("dark_mode");
         change = 1;
@@ -10,4 +26,26 @@ darkToggle.onclick = function() {
         darkMode.classList.remove("dark_mode");
         change = 0;
     }
+}
+
+function loadJSON(){
+    fetch('javaScript/data.json')
+    .then(response => response.json())
+    .then(data => {
+        let html = '';
+        data.forEach(element => {
+            html += `
+            <div class="projects">
+                <div class="project">
+                    <img src="${element.image}">
+                    <h1>${element.name}</h1>
+                    <p>${element.description}</p>
+                    <a href="${element.link}" target="_blank"><button type="button" >Acessar projeto</button></a>
+                </div>
+            </div>
+            `;
+
+        });
+        projects.innerHTML = html;
+    })
 }
