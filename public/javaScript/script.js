8fc26821e59aa6937ef39858b924c146e277cecd
TheLeleo <99210769+TheLeleo@users.noreply.github.com>
2022-05-30 18:45:31 -0300

    Add files via upload

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -3,6 +3,38 @@ const projects = document.querySelector(".projects_section");
 
 
 function loadProjects(data) {
+    fetch('javaScript/data.json')
+    .then(response => response.json())
+    .then(data => {
+        let html = '';
+        data.forEach(element => {
+            html += `
+            <div class="projects">
+                <div class="project">
+                    <img src="${element.image}">
+                    <h1>${element.name}</h1>
+                    <p>${element.description}</p>
+                    <a href="${element.link}" target="_blank"><button type="button" >Acessar projeto</button></a>
+                </div>
+            </div>
+            `;
+
+        });
+        projects.innerHTML = html;
+    })
+}
+
+
+
+function getProjects(){
+    let url = '/getprojects'
+
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("get", url, false)
+    xhttp.send()
+
+    let data = JSON.parse(xhttp.responseText)
+
     let html = '';
         data.forEach(element => {
             console.log(element)
@@ -21,15 +53,8 @@ function loadProjects(data) {
         projects.innerHTML = html;
 }
 
-
-function getProjects(){
-    let url = '/getprojects'
-
-    let xhttp = new XMLHttpRequest()
-    xhttp.open("get", url, false)
-    xhttp.send()
-
-    let data = JSON.parse(xhttp.responseText)
-    loadProjects(data)
+if (window.location.href == "http://localhost:3000/") {
+    getProjects()
+} else {
+    loadProjects()
 }
-getProjects()
